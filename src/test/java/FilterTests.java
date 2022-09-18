836039a0dda796f6d181cd8a12e1830c21b06d00
Diego Sanchez <Diego.Sanchez@endava.com>
2022-09-18 09:43:46 -0500

    Adding 'Sort movies by date of release (Ascending)' test

--- src/test/java/FilterTests.java
+++ src/test/java/FilterTests.java
@@ -5,6 +5,8 @@ import pages.MoviePage;
 import pages.TopRatedPage;
 
 import java.time.Duration;
+import java.util.Date;
+import java.util.List;
 
 public class FilterTests extends Hooks{
 
@@ -17,5 +19,19 @@ public class FilterTests extends Hooks{
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         MoviePage moviePage = topRatedPage.selectAnyFilterResult();
         Assert.assertTrue(moviePage.getMovieGenresList().contains(genreToFilter.toLowerCase()));
+    }
+
+    @Test
+    public void ascendingDatesAfterSort(){
+        MainPage mainPage = new MainPage(driver);
+        TopRatedPage topRatedPage = mainPage.goToTopRated();
+        topRatedPage.sortByDateAscending();
+        int numberOfDates = 4;
+        List<Date> dateResults = topRatedPage.getNDatesFromSortResult(numberOfDates);
+        Boolean areDatesAscending = true;
+        for (int i=0; i < numberOfDates-1; i++){
+            areDatesAscending = areDatesAscending && dateResults.get(i).before(dateResults.get(i+1));
+        }
+        Assert.assertTrue(areDatesAscending);
     }
 }
--- src/test/java/pages/TopRatedPage.java
+++ src/test/java/pages/TopRatedPage.java
@@ -1,15 +1,16 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TopRatedPage extends BasePage{
 
@@ -17,10 +18,18 @@ public class TopRatedPage extends BasePage{
     private WebElement filterDropdown;
     @FindBy(css = "#with_genres a")
     private List<WebElement> allGenreItems;
+    @FindBy(css = "div.apply.small a")
+    private WebElement applyFilterButton1;
     @FindBy(css = "div.apply.full a")
-    private WebElement applyFilterButton;
+    private WebElement applyFilterButton2;
     @FindBy(css = "#page_1>div")
     private List<WebElement> allFilterResults;
+    @FindBy(css = ".k-widget.k-dropdown.kendo_dropdown.full_width.font_size_1")
+    private WebElement sortDropdown;
+    @FindBy(css = "#sort_by_listbox>li:nth-child(6)")
+    private WebElement sortByDateAscendingOption;
+    @FindBy(css = ".card.style_1 p")
+    private List<WebElement> allDatesSortResults;
 
     public TopRatedPage(WebDriver driver) {
         super(driver);
@@ -34,7 +43,7 @@ public class TopRatedPage extends BasePage{
                 break;
             };
         }
-        applyFilterButton.click();
+        applyFilterButton2.click();
     }
 
     public MoviePage selectAnyFilterResult(){
@@ -44,5 +53,27 @@ public class TopRatedPage extends BasePage{
         explicitWait.until(ExpectedConditions.stalenessOf(allFilterResults.get(randomSelection)));
         explicitWait.until(ExpectedConditions.elementToBeClickable(allFilterResults.get(randomSelection))).click();
         return new MoviePage(driver);
+    }
+
+    public void sortByDateAscending(){
+        sortDropdown.click();
+        sortByDateAscendingOption.click();
+        applyFilterButton1.click();
+    }
+
+    public List<Date> getNDatesFromSortResult(int n){
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        explicitWait.until(ExpectedConditions.stalenessOf(allDatesSortResults.get(0)));
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM dd, yyyy");
+        List<Date> selectedDates = new ArrayList<>();
+        for (String textDate : allDatesSortResults.subList(0,n).stream().map(WebElement::getText).collect(Collectors.toList())){
+            try {
+                selectedDates.add(dateFormatter.parse(textDate));
+            }catch (ParseException e){
+                System.out.println("");
+            }
+        }
+        return selectedDates;
     }
 }
